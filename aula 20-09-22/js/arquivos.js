2c897055a7f0b53edabecab89d1fb5efe752c48d
Cleber <104567962+clebspanik@users.noreply.github.com>
2022-09-21 11:07:33 -0300

    Exemplos de eventos: javaScript/validadorDeSenha
    Aula 20/09/22

---
+++ aula 20-09-22/js/arquivos.js
@@ -0,0 +1,8 @@
+function validar () {
+
+    var nome - document.getElementById ('nome').value;
+    var email = document.getElementById ('email').value;
+    var telefone - document.getElementById ('telefone').value;
+    var cpf = document.getElementById ("CPF").value;
+    var senha; document
+}
